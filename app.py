10d23147ef45b4eca5787a1344be099d8646ca45
Rishabh Rastogi <rrishabh@vt.edu>
2022-11-04 03:51:15 -0400

    pushing simulator service code

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from Simulator import fun
+from Simulator import get_simulator_data
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/simulator')
 def simulator_handler():
-    return fun(2)
+    return get_simulator_data(2)
 
 app.run(host='0.0.0.0', port=5000, debug=True)
